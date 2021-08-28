0cb5e59fe239de7b471900ce35c28d7adaa74f24
Aleksei Khabirov <finko85@gmail.com>
2021-08-28 16:05:45 +0300

    feat: add eslint and fix errors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ChangeTypes = {
   ADDED: 'ADDED',
   REMOVED: 'REMOVED',
   UNCHANGED: 'UNCHANGED',
-}
+};
 
 const readFileData = (filepath) => {
   const currentPath = path.resolve(process.cwd(), `${filepath}`);
@@ -17,7 +17,7 @@ const readFileData = (filepath) => {
 };
 
 const formatLine = (type, key, value) => {
-  switch(type) {
+  switch (type) {
     case ChangeTypes.REMOVED:
       return `${' '.repeat(SPACE_COUNT / 2)}- ${key}: ${value}\n`;
     case ChangeTypes.ADDED:
@@ -29,23 +29,16 @@ const formatLine = (type, key, value) => {
   }
 };
 
-// {
-//   - follow: false
-//     host: hexlet.io
-//   - proxy: 123.234.53.22
-//   - timeout: 50
-//   + timeout: 20
-//   + verbose: true
-// }
-
 const buildDiff = (objA, objB) => {
   let result = '';
 
   const keys = _.sortBy(_.union(_.keys(objA), _.keys(objB)));
 
-  keys.forEach((key, index) => {
-    if (index === 0) {
-      result += '{' + '\n';
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+
+    if (i === 0) {
+      result += '{\n';
     }
 
     if (_.has(objA, `${key}`) && !_.has(objB, `${key}`)) {
@@ -54,35 +47,38 @@ const buildDiff = (objA, objB) => {
 
     if (_.has(objA, `${key}`) && _.has(objB, `${key}`)) {
       if (objA[key] === objB[key]) {
-        return result += formatLine(ChangeTypes.UNCHANGED, key, objA[key]);
+        result += formatLine(ChangeTypes.UNCHANGED, key, objA[key]);
+      } else {
+        result += formatLine(ChangeTypes.REMOVED, key, objA[key]);
+        result += formatLine(ChangeTypes.ADDED, key, objB[key]);
       }
-
-      result += formatLine(ChangeTypes.REMOVED, key, objA[key]);
-      result += formatLine(ChangeTypes.ADDED, key, objB[key]);
     }
 
     if (!_.has(objA, `${key}`) && _.has(objB, `${key}`)) {
       result += formatLine(ChangeTypes.ADDED, key, objB[key]);
     }
 
-    if (index === keys.length - 1) {
+    if (i === keys.length - 1) {
       result += '}';
     }
-  })
+  }
 
   return result;
-}
+};
 
 export default (filepath1, filepath2) => {
   const file1 = readFileData(filepath1);
   const file2 = readFileData(filepath2);
 
   const type = path.extname(filepath1).slice(1);
+  let diff = '';
 
   if (type === 'json') {
     const obj1 = JSON.parse(file1);
     const obj2 = JSON.parse(file2);
 
-    return buildDiff(obj1, obj2);
+    diff = buildDiff(obj1, obj2);
   }
-}
+
+  return diff;
+};
