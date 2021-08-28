6287f2892a4e53fa16f14b47d62629dcc2f44870
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-08-28 14:31:28 +0300

    feat: add read files data fn

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]')
   .arguments('<filepath1> <filepath2>')
+  .action((path1, path2) => {
+    console.log(genDiff(path1, path2));
+  })
   .parse()
