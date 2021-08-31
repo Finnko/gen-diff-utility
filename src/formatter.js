a3188bbda3367261cb09966d60dba8f411c9e564
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-08-31 23:56:06 +0300

    feat: add stylish formatter for simple objects

--- src/formatter.js
+++ src/formatter.js
@@ -2,15 +2,28 @@ import ChangeTypes from './const.js';
 
 const SPACE_COUNT = 4;
 
-export default (type, key, value) => {
-  switch (type) {
-    case ChangeTypes.REMOVED:
-      return `${' '.repeat(SPACE_COUNT / 2)}- ${key}: ${value}\n`;
-    case ChangeTypes.ADDED:
-      return `${' '.repeat(SPACE_COUNT / 2)}+ ${key}: ${value}\n`;
-    case ChangeTypes.UNCHANGED:
-      return `${' '.repeat(SPACE_COUNT)}${key}: ${value}\n`;
-    default:
-      throw new Error(`Unexpected type ${type}`);
-  }
-};
+export default function formatDiff(diff, indent = SPACE_COUNT) {
+  const styledDiff = diff.map((node) => {
+    const {
+      type,
+      key,
+    } = node;
+
+    switch (type) {
+      case ChangeTypes.REMOVED:
+        return `${' '.repeat(indent - 2)}- ${key}: ${node.value}\n`;
+      case ChangeTypes.ADDED:
+        return `${' '.repeat(indent - 2)}+ ${key}: ${node.value}\n`;
+      case ChangeTypes.UNCHANGED:
+        return `${' '.repeat(indent)}${key}: ${node.value}\n`;
+      case ChangeTypes.UPDATED:
+        return `${' '.repeat(indent - 2)}- ${key}: ${node.value1}\n${' '.repeat(indent - 2)}+ ${key}: ${node.value2}\n`;
+      case ChangeTypes.WITH_CHILDREN:
+        return `${' '.repeat(indent)}${key}: ${formatDiff(node.children, indent * 2)}\n`;
+      default:
+        throw new Error(`Unexpected type ${type}`);
+    }
+  });
+
+  return `{\n${styledDiff.join('')}${' '.repeat(indent - 4)}}`;
+}
