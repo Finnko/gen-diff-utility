21a920deb1aa93204600d790f42e70443dc00307
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-09-01 23:30:38 +0300

    fix: indent in formatter

--- src/formatter.js
+++ src/formatter.js
@@ -2,12 +2,19 @@ import ChangeTypes from './const.js';
 
 const SPACE_COUNT = 4;
 
+// const formatObjectToString = (value) => {
+//   if (ob)
+//
+// }
+
 export default function formatDiff(diff, indent = SPACE_COUNT) {
   const styledDiff = diff.map((node) => {
     const {
       type,
       key,
     } = node;
+
+    console.log({indent})
 
     switch (type) {
       case ChangeTypes.REMOVED:
@@ -19,7 +26,7 @@ export default function formatDiff(diff, indent = SPACE_COUNT) {
       case ChangeTypes.UPDATED:
         return `${' '.repeat(indent - 2)}- ${key}: ${node.value1}\n${' '.repeat(indent - 2)}+ ${key}: ${node.value2}\n`;
       case ChangeTypes.WITH_CHILDREN:
-        return `${' '.repeat(indent)}${key}: ${formatDiff(node.children, indent * 2)}\n`;
+        return `${' '.repeat(indent)}${key}: ${formatDiff(node.children, indent + 4)}\n`;
       default:
         throw new Error(`Unexpected type ${type}`);
     }
